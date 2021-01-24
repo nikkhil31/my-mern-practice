6f03b76137a3e31fddce4ba4fbf6e30fe3e23164
Nikhil Limbad <nikhilimbad@gmail.com>
2021-01-24 15:23:29 +0530

    backend jwt done

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,12 +1,12 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`)
     res.status(404)
+    const error = new Error(`Not Found - ${req.originalUrl}`)
     next(error)
 }
 
-
 const errorHandler = (err, req, res, next) => {
     const error = res.statusCode === 200 ? 500 : res.statusCode
+    console.log(error)
     res.status(error)
 
     res.json({

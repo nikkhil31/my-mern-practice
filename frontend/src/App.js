81cfff63eb21a8e2ddb1d09eec267d2d1dab7b81
Nikhil Limbad <nikhilimbad@gmail.com>
2021-01-31 18:38:01 +0530

    front end auth,update,profile done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => {
-  return (
-    <Router>
-      <Header />
-      <main>
-        <Container>
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/product/:id" component={ProductScreen} exact />
-          <Route path="/cart/:id?" component={CartScreen} exact />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
-};
+    return (
+        <Router>
+            <Header />
+            <main>
+                <Container>
+                    <Route
+                        path="/product/:id"
+                        component={ProductScreen}
+                        exact
+                    />
+                    <Route path="/login" component={LoginScreen} />
+                    <Route path="/register" component={RegisterScreen} />
+                    <Route path="/profile" component={ProfileScreen} />
+                    <Route path="/cart/:id?" component={CartScreen} exact />
+                    <Route path="/" component={HomeScreen} exact />
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    )
+}
 
-export default App;
+export default App
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,34 @@ import {
     productListReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailReducer,
+    userUpdateProfileReducer,
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
+
 const initialState = {
     cart: { cartItem: cartItemFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
